2dbf4b1d782165b0fdf95681c620eda0ecdc622d
KrzychuJedi <tharealiestjedi@gmail.com>
2019-08-04 19:00:00 +0200

    code & configuration

---
+++ src/main/java/org/websparrow/HelloController.java
@@ -0,0 +1,23 @@
+package org.websparrow;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HelloController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String name = request.getParameter("name");
+
+		Map<String, String> map = new HashMap<>();
+		map.put("name", name);
+
+		return new ModelAndView("welcome", map);
+	}
+
+}
